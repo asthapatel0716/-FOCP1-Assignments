020a6e9ff4827a112b5ffa17c1a4ecd173f071ff
asthapatel0716 <asthapatel0716@gmail.com>
2025-11-02 22:17:57 +0530

    Create Q6.c

--- FOCP1-ASSIGNMENT/Q6.c
+++ FOCP1-ASSIGNMENT/Q6.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
-#include <math.h>
-int binaryToDecimal(long long binary) 
-{
-    int decimal = 0, i = 0, remainder;
-    while (binary != 0) {
-        remainder = binary % 10;
-        decimal += remainder * pow(2, i);
-        binary /= 10;
-        i++;
-    }
-    return decimal;
-}
-long long decimalToBinary(int decimal) 
-{
-    long long binary = 0;
-    int remainder, i = 1;
 
-    while (decimal != 0) {
-        remainder = decimal % 2;
-        decimal /= 2;
-        binary += remainder * i;
-        i *= 10;
-    }
-    return binary;
-}
-
-int main() {
+int main()
+{
     int choice;
-    long long binary;
-    int decimal;
+
     printf("1. Binary to Decimal\n");
     printf("2. Decimal to Binary\n");
-    printf("Enter your choice (1 or 2): ");
+    printf("Enter choice: ");
     scanf("%d", &choice);
 
-    if (choice == 1) {
-        printf("Enter a binary number: ");
-        scanf("%lld", &binary);
-        printf("Decimal equivalent: %d\n", binaryToDecimal(binary));
+    if (choice == 1)
+    {
+        long long bin;
+        int dec = 0, base = 1;
+        printf("Enter binary: ");
+        scanf("%lld", &bin);
+
+        while (bin > 0)
+        {
+            int lastDigit = bin % 10;
+            dec += lastDigit * base;
+            bin /= 10;
+            base *= 2;
+        }
+
+        printf("Decimal = %d\n", dec);
     }
-    else if (choice == 2) {
-        printf("Enter a decimal number: ");
-        scanf("%d", &decimal);
-        printf("Binary equivalent: %lld\n", decimalToBinary(decimal));
+    else if (choice == 2)
+    {
+        int dec;
+        long long bin = 0, base = 1;
+        printf("Enter decimal: ");
+        scanf("%d", &dec);
+
+        while (dec > 0)
+        {
+            int rem = dec % 2;
+            bin += rem * base;
+            dec /= 2;
+            base *= 10;
+        }
+
+        printf("Binary = %lld\n", bin);
     }
-    else {
-        printf("Invalid choice! Please enter 1 or 2.\n");
+    else
+    {
+        printf("Invalid choice\n");
     }
 
     return 0;

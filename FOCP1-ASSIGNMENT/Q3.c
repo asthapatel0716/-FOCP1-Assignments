2beb34da16cc04721ce9df9f3b40f529d67c345d
asthapatel0716 <asthapatel0716@gmail.com>
2025-11-02 22:10:30 +0530

    Create Q3.c

--- FOCP1-ASSIGNMENT/Q3.c
+++ FOCP1-ASSIGNMENT/Q3.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
-int add(int a, int b)
- {
-    int carry;
-    while (b != 0) {
-        carry = a & b; 
-        a = a ^ b;       
-        b = carry << 1; 
-    }
-    return a;
-}
-int subtract(int a, int b) {
-    return add(a, add(~b, 1));
-}
 
-int main() {
-    int num1, num2;
-
-    printf("Enter two integers: ");
-    scanf("%d %d", &num1, &num2);
-
-    printf("Result of %d - %d = %d\n", num1, num2, subtract(num1, num2));
+int main()
+{
+    int x, y;
+    printf("Enter number 1: ");
+    scanf("%d", &x);
+    printf("Enter number 2: ");
+    scanf("%d", &y);
+    printf("The subtraction of the numbers would be %d", x + (~y + 1));
     return 0;
 }

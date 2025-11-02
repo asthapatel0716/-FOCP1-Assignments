803981e206bc63832ce54bdaa60285fd1255a83a
asthapatel0716 <asthapatel0716@gmail.com>
2025-11-02 17:29:29 +0530

    Create Q6.c

---
+++ FOCP1-ASSIGNMENT/Q6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+int binaryToDecimal(long long binary) 
+{
+    int decimal = 0, i = 0, remainder;
+    while (binary != 0) {
+        remainder = binary % 10;
+        decimal += remainder * pow(2, i);
+        binary /= 10;
+        i++;
+    }
+    return decimal;
+}
+long long decimalToBinary(int decimal) 
+{
+    long long binary = 0;
+    int remainder, i = 1;
+
+    while (decimal != 0) {
+        remainder = decimal % 2;
+        decimal /= 2;
+        binary += remainder * i;
+        i *= 10;
+    }
+    return binary;
+}
+
+int main() {
+    int choice;
+    long long binary;
+    int decimal;
+    printf("1. Binary to Decimal\n");
+    printf("2. Decimal to Binary\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter a binary number: ");
+        scanf("%lld", &binary);
+        printf("Decimal equivalent: %d\n", binaryToDecimal(binary));
+    }
+    else if (choice == 2) {
+        printf("Enter a decimal number: ");
+        scanf("%d", &decimal);
+        printf("Binary equivalent: %lld\n", decimalToBinary(decimal));
+    }
+    else {
+        printf("Invalid choice! Please enter 1 or 2.\n");
+    }
+
+    return 0;
+}

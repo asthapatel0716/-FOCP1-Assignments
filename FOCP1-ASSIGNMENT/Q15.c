0561168e341dd43a332679dde8dae3f0c4db890d
asthapatel0716 <asthapatel0716@gmail.com>
2025-11-02 21:48:50 +0530

    Update Q15.c

--- FOCP1-ASSIGNMENT/Q15.c
+++ FOCP1-ASSIGNMENT/Q15.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() 
 {
-    int n,i,last;
+    int n,i;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
     int arr[n];
@@ -10,17 +10,17 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    last = arr[n - 1];
-    for (i = n - 1; i > 0; i--) 
+ int temp = arr[4]; 
+    for(i = 4; i > 0; i--)
     {
         arr[i] = arr[i - 1];
     }
-    arr[0] = last;
-    printf("\nArray after one clockwise rotation:\n");
-    for (i = 0; i < n; i++) 
+    arr[0] = temp; 
+    printf("The new array is: ");
+    for(i = 0; i < 5; i++)
     {
         printf("%d ", arr[i]);
     }
-    printf("\n");
+
     return 0;
 }
